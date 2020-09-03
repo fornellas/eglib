5af727a91f30a8ca316b8d1725567ed5b973361e
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-09-03 22:58:37 +0100

    Add function defaults

--- src/eglib.c
+++ src/eglib.c
@@ -1,5 +1,18 @@
 #include "eglib.h"
 
+static void default_display_power_up(void *display_config) {}
+static void default_display_power_down(void *display_config) {}
+static void default_display_get_dimension(void *display_config, eglib_coordinate_t *width, eglib_coordinate_t*height) {}
+static void default_display_draw_pixel(void *display_config, eglib_coordinate_t x, eglib_coordinate_t y, eglib_color_t color) {}
+
+static void default_comm_power_up(void *comm_config, uint16_t clock_ns) {}
+static void default_comm_power_down(void *comm_config) {}
+static void default_comm_delay(void *comm_config, uint16_t microseconds) {}
+static void default_comm_set_reset(void *comm_config, uint8_t state) {}
+static void default_comm_set_cd(void *comm_config, uint8_t state) {}
+static void default_comm_set_cs(void *comm_config, uint8_t state) {}
+static void default_comm_send_byte(void *comm_config, uint8_t state) {}
+
 void eglib_Init(
 	eglib_t *eglib,
 	const eglib_display_t *display,
@@ -9,11 +22,31 @@ void eglib_Init(
 ) {
 	eglib->display = *display;
 	eglib->display_config = display_config;
-	// TODO check for NULL function pointers and set default function
+	if(NULL == eglib->display.power_up)
+		eglib->display.power_up = default_display_power_up;
+	if(NULL == eglib->display.power_down)
+		eglib->display.power_down = default_display_power_down;
+	if(NULL == eglib->display.get_dimension)
+		eglib->display.get_dimension = default_display_get_dimension;
+	if(NULL == eglib->display.draw_pixel)
+		eglib->display.draw_pixel = default_display_draw_pixel;
 
 	eglib->comm = *comm;
 	eglib->comm_config = comm_config;
-	// TODO check for NULL function pointers and set default function
+	if(NULL == eglib->comm.power_up)
+		eglib->comm.power_up = default_comm_power_up;
+	if(NULL == eglib->comm.power_down)
+		eglib->comm.power_down = default_comm_power_down;
+	if(NULL == eglib->comm.delay)
+		eglib->comm.delay = default_comm_delay;
+	if(NULL == eglib->comm.set_reset)
+		eglib->comm.set_reset = default_comm_set_reset;
+	if(NULL == eglib->comm.set_cd)
+		eglib->comm.set_cd = default_comm_set_cd;
+	if(NULL == eglib->comm.set_cs)
+		eglib->comm.set_cs = default_comm_set_cs;
+	if(NULL == eglib->comm.send_byte)
+		eglib->comm.send_byte = default_comm_send_byte;
 
 	eglib->clip.x = 0;
 	eglib->clip.y = 0;

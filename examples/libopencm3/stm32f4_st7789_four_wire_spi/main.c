fc1c1835de5170d8dd26563031a60dab011d36a4
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-11-24 13:34:43 +0000

    Update example

--- examples/libopencm3/stm32f4_st7789_four_wire_spi/main.c
+++ examples/libopencm3/stm32f4_st7789_four_wire_spi/main.c
@@ -1,5 +1,6 @@
 #include <eglib.h>
 #include <eglib/hal/four_wire_spi/libopencm3_stm32f4.h>
+#include <eglib/display/frame_buffer.h>
 #include <eglib/display/st7789.h>
 #include <stdio.h>
 #include <libopencm3/stm32/rcc.h>
@@ -36,7 +37,7 @@ int main(void) {
 	st7789_config_t st7789_config = {
 		.width = 240,
 		.height = 240,
-		.color = ST7789_COLOR_18_BIT,
+		.color = ST7789_COLOR_16_BIT,
 		.page_address = ST7789_PAGE_ADDRESS_TOP_TO_BOTTOM,
 		.colum_address = ST7789_COLUMN_ADDRESS_LEFT_TO_RIGHT,
 		.page_column_order = ST7789_PAGE_COLUMN_ORDER_NORMAL,
@@ -46,26 +47,43 @@ int main(void) {
 
 	rcc_clock_setup_pll(&rcc_hse_25mhz_3v3[RCC_CLOCK_3V3_84MHZ]);
 
-	eglib_Init(
-		&eglib,
+	// eglib_Init(
+	// 	&eglib,
+	// 	&four_wire_spi_libopencm3_stm32f4, &four_wire_spi_libopencm3_stm32f4_config,
+	// 	&st7789, &st7789_config
+	// );
+
+	frame_buffer_config_t frame_buffer_config;
+	eglib_Init_FrameBuffer(
+		&eglib, &frame_buffer_config,
 		&four_wire_spi_libopencm3_stm32f4, &four_wire_spi_libopencm3_stm32f4_config,
 		&st7789, &st7789_config
 	);
 
-
-	for(uint16_t y=0;y<240;y++) {
+	for(uint16_t y=0;y<st7789_config.width;y++) {
 		uint8_t v;
-		v = y / 240.0 * 255;
+		v = y / (float)st7789_config.width * 255;
 		eglib_SetColor(&eglib, 0, v, v, v);
-		eglib_DrawLine(&eglib,0, y, 239, y);
+		eglib_DrawLine(&eglib,0, y, st7789_config.width-1, y);
 	}
 
 	eglib_SetColor(&eglib, 0, 255, 0, 0);
-	eglib_DrawLine(&eglib,0, 0, 239, 239);
+	eglib_DrawLine(&eglib,0, 0, st7789_config.width-1, st7789_config.height/2);
 
 	eglib_SetColor(&eglib, 0, 0, 255, 0);
-	eglib_DrawLine(&eglib,0, 0, 239/2, 239);
+	eglib_DrawLine(&eglib,0, 0, st7789_config.width-1, st7789_config.height-1);
 
 	eglib_SetColor(&eglib, 0, 0, 0, 255);
-	eglib_DrawLine(&eglib,0, 0, 239, 239/2);
+	eglib_DrawLine(&eglib,0, 0, st7789_config.width/2, st7789_config.height-1);
+
+	eglib_FrameBuffer_Send(
+		&eglib,
+		st7789_config.width / 8, st7789_config.height / 8,
+		3 * st7789_config.width / 4, 3 * st7789_config.height / 4
+	);
+	// eglib_FrameBuffer_Send(
+	// 	&eglib,
+	// 	0, 0,
+	// 	st7789_config.width - 1, st7789_config.height - 1
+	// );
 }

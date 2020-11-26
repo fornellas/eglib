ac2214559fafd832c26c0f6f8268ee7ba5f96d0c
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-11-26 22:48:57 +0000

    Tweak theme

--- docs/conf.py
+++ docs/conf.py
@@ -19,12 +19,12 @@ import os.path
 
 # -- Project information -----------------------------------------------------
 
-project = 'eglib'
-copyright = '2020, Fabio Pugliese Ornellas'
-author = 'Fabio Pugliese Ornellas'
+project = "eglib"
+copyright = "2020, Fabio Pugliese Ornellas"
+author = "Fabio Pugliese Ornellas"
 
 # The full version, including alpha/beta/rc tags
-release = 'dev'
+release = "dev"
 
 
 # -- General configuration ---------------------------------------------------
@@ -32,20 +32,18 @@ release = 'dev'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-	"hawkmoth"
-]
+extensions = ["hawkmoth"]
 
 # hawkmoth
 cautodoc_root = os.path.abspath(os.path.dirname(__file__) + "/../src/")
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -53,18 +51,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
-html_sidebars = {
-    "**": [
-        "globaltoc.html",  # a coarse-grained table of contents for the whole documentation set, collapsed
-        "searchbox.html",  # the “quick search” box
-    ],
+html_theme = "alabaster"
+html_theme_options = {
+    "logo": "logo.png",
+    # "donate_url": "http://tbd",
+    "github_banner": True,
+    "github_button": True,
+    "github_type": "star",
+    "github_user": "fornellas",
+    "github_repo": "eglib",
+    "travis_button": True,
+    "show_relbar_bottom": True,
+    "code_font_size": "0.8em",
 }
-
-html_logo = "../assets/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- docs/index.rst
+++ docs/index.rst
@@ -46,5 +46,6 @@ Features
 .. toctree::
    :maxdepth: 4
    :caption: Contents:
+   :hidden:
 
    reference/index.rst

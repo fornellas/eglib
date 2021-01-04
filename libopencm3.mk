9832b92aa5bb8c61e76044c79e7003c8fc8de9c4
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-01-04 22:33:55 +0000

    Add eaxmples to autotools

--- libopencm3.mk
+++ libopencm3.mk
@@ -2,4 +2,8 @@ include $(OPENCM3_DIR)/mk/genlink-config.mk
 
 .PHONY: CPPFLAGS
 CPPFLAGS:
-	@echo $(CPPFLAGS) $(ARCH_FLAGS)
+	@echo $(CPPFLAGS)
+
+.PHONY: ARCH_FLAGS
+ARCH_FLAGS:
+	@echo $(ARCH_FLAGS)
--- tests/common.mk
+++ tests/common.mk
@@ -1,4 +1,1 @@
-EGLIB_CFLAGS = -I$(top_builddir)/src -I$(top_srcdir)/src
-EGLIB_LIBS = -leglib -L$(top_builddir)/src/ -lm
-
 TEST_LOG_DRIVER = env AM_TAP_AWK='$(AWK)' $(SHELL) $(top_srcdir)/build-aux/tap-driver.sh
